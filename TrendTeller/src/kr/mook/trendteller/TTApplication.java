df403c2ff24e506c2257e33d86d5d1e595bc2a64
bbangzum <ryukw82@gmail.com>
2013-02-01 13:43:27 +0900

    initial commit

---
+++ TrendTeller/src/kr/mook/trendteller/TTApplication.java
@@ -0,0 +1,12 @@
+package kr.mook.trendteller;
+
+import org.holoeverywhere.ThemeManager;
+import org.holoeverywhere.app.Application;
+
+public class TTApplication extends Application {
+
+	static {
+		ThemeManager.setDefaultTheme(ThemeManager.DARK);
+	}
+
+}
