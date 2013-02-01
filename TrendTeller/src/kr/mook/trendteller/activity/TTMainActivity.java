9dc136d8723157fe8e4a44aa71ad3cc38a5d082d
bbangzum <ryukw82@gmail.com>
2013-02-01 14:44:16 +0900

    fix AndroidManifest.xml

--- TrendTeller/src/kr/mook/trendteller/activity/TTMainActivity.java
+++ TrendTeller/src/kr/mook/trendteller/activity/TTMainActivity.java
@@ -4,6 +4,8 @@ import kr.mook.trendteller.R;
 import android.os.Bundle;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 
 public class TTMainActivity extends SherlockFragmentActivity {
 
@@ -11,6 +13,30 @@ public class TTMainActivity extends SherlockFragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add("ADD").setShowAsAction(
+				MenuItem.SHOW_AS_ACTION_IF_ROOM
+						| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		menu.add("?").setShowAsAction(
+				MenuItem.SHOW_AS_ACTION_IF_ROOM
+						| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		if (item.getTitle().equals("ADD")) {
+
+		} else if (item.getTitle().equals("?")) {
+
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 }
